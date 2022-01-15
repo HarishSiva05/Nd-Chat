f3dbd95c7d5d77dab6e4fc1e5cdcd1d6855eebc4
unknown <harishsivaram@icloud.com>
2022-01-15 15:45:34 +0530

    Session Parser installed - the session is not getting the color it throws refrence error

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+//const cookieParser = require('cookie-parser');
+const { Session } = require('inspector');
+
 var express = require('express'),
     app= express(),
-    path = require('path')
+    path = require('path'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session')
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('hogan-express'))
 app.set('view engine','html');
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cookieParser());
+app.use(session({secret:'Avengers'}));
 require('./routes/routes.js')(express, app);
 
 app.listen(3000,function(){
--- routes/routes.js
+++ routes/routes.js
@@ -10,5 +10,14 @@ module.exports = function(express, app) {
     router.get('/chatroom',function(req, res, next){
         res.render('chatroom',{title:'Chatrooms'});
     })
+
+    router.get('/setcolor', function(req, res, next){
+        req.session.favColor = "Green"
+        res.send('Setting the Color');
+    })
+
+    router.get('/getcolor', function(req, res, next){
+        res.send('Color' + (req.session.favcolor===undefinded?'Not Found':req.session.favColor))
+    })
     app.use('/', router);
 }
